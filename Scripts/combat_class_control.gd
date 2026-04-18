f54e6b671e06933cdc5f41c9af8bc8f3f52fe42c
PoweredDevState <jadervin13@gmail.com>
2026-04-18 13:23:18 -0500

    Added selling to game
    Note: Need to add the function to increase coins when selling.

--- Scripts/combat_class_control.gd
+++ Scripts/combat_class_control.gd
@@ -1,11 +1,11 @@
 extends Control
 class_name CombatUnitControl
 
-
 @export var damage_label: Label
 @export var health_label: Label
-@export var name_label:Label
+@export var name_label : Label
 var effect : EffectData
+var unit_data : UnitData
 
 ## This variable has a set function that changes the attack text
 var attack : int:
@@ -20,10 +20,15 @@ var health : int:
 		health_label.text = str(health)
 
 
-
 ## This sets up the unit data for the unit
 func dress(data:UnitData):
 	attack = data.attack
 	health = data.health
 	name_label.text = data.unit_name
 	effect = data.effect
+	unit_data = data
+
+
+func _on_sell_button_pressed() -> void:
+	PlayerUnitsContainer.remove_unit_from_list(unit_data)
+	queue_free()
