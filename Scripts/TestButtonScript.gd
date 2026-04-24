39ac2a586f89c843566afab04860269acd56d5a4
PoweredDevState <jadervin13@gmail.com>
2026-04-24 13:51:34 -0500

    Made title screen

--- Scripts/TestButtonScript.gd
+++ Scripts/TestButtonScript.gd
@@ -1,7 +1,14 @@
 extends Button
 
 @export var transition_scene_file_path : String
+var current_scene : String
+
+func _ready() -> void:
+	current_scene = get_tree().current_scene.name
 
 func _on_pressed() -> void:
-	if PlayerUnitsContainer.ally_unit_list.size() != 0:
+	if current_scene == "ShopScene":
+		if PlayerUnitsContainer.ally_unit_list.size() != 0:
+			get_tree().change_scene_to_file(transition_scene_file_path)
+	else:
 		get_tree().change_scene_to_file(transition_scene_file_path)
